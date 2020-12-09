5648badb67700aa26d90e7f4df4476030a47ee6a
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2020-12-09 18:09:45 +0100

    Initial commit

---
+++ src/lotto/LottoDemo.java
@@ -0,0 +1,16 @@
+package lotto;
+
+
+import java.util.Random;
+
+public class LottoDemo {
+
+    public static void main(String[] args) {
+
+        // Schreibe eine statische Methode, welche ein int-Array mit 6 Zufallszahlen
+        // (Lotto-Tip) liefert.
+        // Stelle dabei sicher, dass jede Zahl nur einmal vorkommt und somit eindeutig ist.
+
+
+    }
+}
